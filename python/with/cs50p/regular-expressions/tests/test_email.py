48bfb8a67c2e56a95eba145ada2d693fb5072a02
ahmetcanisik <can.isik.business@gmail.com>
2025-03-28 16:01:41 +0300

    one false test in truthy test, thats fixed

--- python/with/cs50p/regular-expressions/tests/test_email.py
+++ python/with/cs50p/regular-expressions/tests/test_email.py
@@ -4,12 +4,12 @@ def test_is_valid_email():
     # These tests are expected "truthy"
     assert is_valid_email("imcanisik@gmail.com") == True
     assert is_valid_email("imcanisik@gmail.edu") == True
-    assert is_valid_email("imcanisik@gmailcom") == False
     assert is_valid_email("malan@cs50.harvard.edu") == True
     assert is_valid_email("ahmet@a.eduroam.edu") == True
     
     # These tests are expected "falsy"
     assert is_valid_email("@gmail.com") == False
     assert is_valid_email("gmail.com") == False
+    assert is_valid_email("imcanisik@gmailcom") == False
     assert is_valid_email("imcanisik@gmail?com") == False
     assert is_valid_email("imcanisik@@@gmail.com") == False
